2d902fda5b20849c1a8eae8c300ffb091458a791
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-05-12 10:00:51 +0630

    added getCode api to treatmentVoucher

--- app/controllers/treatmentVoucherController.js
+++ app/controllers/treatmentVoucherController.js
@@ -43,6 +43,23 @@ exports.getTreatmentVoucher = async (req, res) => {
     return res.status(200).send({ success: true, data: result });
 };
 
+exports.getCode = async (req,res) => {
+    let data = {}
+    try {
+        console.log('here')
+        let today = new Date().toISOString()
+        const latestDocument = await TreatmentVoucher.find({}, { seq: 1 }).sort({ _id: -1 }).limit(1).exec();
+        if (latestDocument[0].seq === undefined) data = { ...data, seq: 1, code: "TVC-" + today.split('T')[0].replace(/-/g, '') + "-1" } // if seq is undefined set initial patientID and seq
+        if (latestDocument[0].seq) {
+            const increment = latestDocument[0].seq + 1
+            data = { ...data, code: "TVC-" + today.split('T')[0].replace(/-/g, '') + "-" + increment, seq: increment }
+        }
+        return res.status(200).send({success:true, data:data})
+    } catch (error) {
+        return res.status(500).send({ "error": true, message: error.message })
+    }
+}
+
 exports.getRelatedTreatmentVoucher = async (req, res) => {
     try {
         let query = { isDeleted: false };
--- app/routes/treatmentVoucherRoutes.js
+++ app/routes/treatmentVoucherRoutes.js
@@ -2,12 +2,12 @@
 
 const treatmentVoucher = require("../controllers/treatmentVoucherController");
 const { catchError } = require("../lib/errorHandler");
-const  verifyToken= require('../lib/verifyToken');
+const verifyToken = require('../lib/verifyToken');
 
 module.exports = (app) => {
 
     app.route('/api/treatment-voucher')
-        .post( catchError(treatmentVoucher.createTreatmentVoucher))
+        .post(catchError(treatmentVoucher.createTreatmentVoucher))
         .put(catchError(treatmentVoucher.updateTreatmentVoucher))
 
     app.route('/api/treatment-voucher/:id')
@@ -15,9 +15,10 @@ module.exports = (app) => {
         .delete(catchError(treatmentVoucher.deleteTreatmentVoucher))
         .post(catchError(treatmentVoucher.activateTreatmentVoucher))
 
-    app.route('/api/treatment-vouchers').get( catchError(treatmentVoucher.listAllTreatmentVouchers))
+    app.route('/api/treatment-vouchers').get(catchError(treatmentVoucher.listAllTreatmentVouchers))
     app.route('/api/treatment-vouchers/search')
         .post(catchError(treatmentVoucher.searchTreatmentVoucher))
     app.route('/api/treatment-vouchers/filter')
         .post(catchError(treatmentVoucher.getRelatedTreatmentVoucher))
+    app.route('/api/treatment-vouchers/code').get(catchError(treatmentVoucher.getCode))
 };
