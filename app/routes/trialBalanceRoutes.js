88ac753962711552ff46a86163338f7a05a3e086
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-06-08 13:19:33 +0630

    here

--- app/routes/trialBalanceRoutes.js
+++ app/routes/trialBalanceRoutes.js
@@ -7,8 +7,8 @@ const verifyToken = require('../lib/verifyToken');
 module.exports = (app) => {
 
     app.route('/api/trial-balance')
-        .get(verifyToken,catchError(trialBalance.trialBalance))
+        .get(catchError(trialBalance.trialBalance))
         
     app.route('/api/trial-balance/type')
-        .get(verifyToken, catchError(trialBalance.trialBalanceWithType))
+        .get( catchError(trialBalance.trialBalanceWithType))
 };
