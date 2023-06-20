3a2ba35919b9666e5803eb538df3cb9c73b19a28
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-06-20 16:04:19 +0630

    here

--- app/routes/profitAndLossRoutes.js
+++ app/routes/profitAndLossRoutes.js
@@ -5,10 +5,13 @@ const { catchError } = require("../lib/errorHandler");
 const verifyToken = require('../lib/verifyToken');
 
 module.exports = (app) => {
-    app.route('/api/profit-and-loss').get( catchError(profitAndLoss.listAllLog))
-    app.route('/api/profit-and-losses/week').post( catchError(profitAndLoss.getWeek))
-    app.route('/api/profit-and-losses/month').post( catchError(profitAndLoss.getMonth))
-    app.route('/api/profit-and-losses/day').post( catchError(profitAndLoss.getDay))
-    app.route('/api/profit-and-losses/total').get( catchError(profitAndLoss.getTotal))
+    app.route('/api/profit-and-loss').get(catchError(profitAndLoss.listAllLog))
+    app.route('/api/profit-and-losses/week').post(catchError(profitAndLoss.getWeek))
+    app.route('/api/profit-and-losses/month').post(catchError(profitAndLoss.getMonth))
+    app.route('/api/profit-and-losses/day').post(catchError(profitAndLoss.getDay))
+    app.route('/api/profit-and-losses/total').get(catchError(profitAndLoss.getTotal))
+    app.route('/api/profit-and-losses/total/branch').get(catchError(profitAndLoss.getTotalwithBranch))
+    app.route('/api/profit-and-losses/total-filter').get(catchError(profitAndLoss.getTotalWithDateFilter))
+
     // app.route('/api/profit-and-losses/month').post(catchError(profitAndLoss.createUsage))
 };
