e95030e62939d88180130f5639da2da4ef69b06c
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-07-04 16:47:05 +0630

    here

--- app/controllers/historyController.js
+++ app/controllers/historyController.js
@@ -3,20 +3,20 @@ const History = require('../models/history');
 
 exports.listAllHistories = async (req, res) => {
   try {
-    let result = await History.find({isDeleted:false}).populate('relatedPatient')
-    let count = await History.find({isDeleted:false}).count();
+    let result = await History.find({ isDeleted: false }).populate('relatedPatient')
+    let count = await History.find({ isDeleted: false }).count();
     res.status(200).send({
       success: true,
       count: count,
       data: result
     });
   } catch (error) {
-    return res.status(500).send({ error:true, message:'No Record Found!'});
+    return res.status(500).send({ error: true, message: 'No Record Found!' });
   }
 };
 
 exports.getHistory = async (req, res) => {
-  const result = await History.find({ _id: req.params.id,isDeleted:false }).populate('relatedPatient')
+  const result = await History.find({ _id: req.params.id, isDeleted: false }).populate('relatedPatient')
   if (!result)
     return res.status(500).json({ error: true, message: 'No Record Found' });
   return res.status(200).send({ success: true, data: result });
@@ -24,6 +24,7 @@ exports.getHistory = async (req, res) => {
 
 exports.createHistory = async (req, res, next) => {
   try {
+    console.log(req.body)
     const newHistory = new History(req.body);
     const result = await newHistory.save();
     res.status(200).send({
@@ -79,10 +80,10 @@ exports.activateHistory = async (req, res, next) => {
 exports.filterHistories = async (req, res, next) => {
   try {
     let query = {}
-    const { name,code } = req.query
+    const { name, code } = req.query
     if (name) query.name = name
     if (code) query.code = code
-    if (Object.keys(query).length === 0) return res.status(404).send({error:true, message: 'Please Specify A Query To Use This Function'})
+    if (Object.keys(query).length === 0) return res.status(404).send({ error: true, message: 'Please Specify A Query To Use This Function' })
     const result = await History.find(query).populate('relatedPatient')
     if (result.length === 0) return res.status(404).send({ error: true, message: "No Record Found!" })
     res.status(200).send({ success: true, data: result })
@@ -95,7 +96,7 @@ exports.searchHistories = async (req, res, next) => {
   try {
     console.log(req.body.search)
     const result = await History.find({ $text: { $search: req.query.search } }).populate('relatedPatient')
-    if (result.length===0) return res.status(404).send({error:true, message:'No Record Found!'})
+    if (result.length === 0) return res.status(404).send({ error: true, message: 'No Record Found!' })
     return res.status(200).send({ success: true, data: result })
   } catch (err) {
     return res.status(500).send({ error: true, message: err.message })
--- app/routes/historyRoutes.js
+++ app/routes/historyRoutes.js
@@ -3,12 +3,13 @@
 const history = require("../controllers/historyController");
 const { catchError } = require("../lib/errorHandler");
 const verifyToken = require('../lib/verifyToken');
+const upload = require('../lib/fieldUploader').upload;
 
 module.exports = (app) => {
 
     app.route('/api/history')
-        .post(verifyToken, catchError(history.createHistory))
-        .put(verifyToken, catchError(history.updateHistory))
+        .post(upload, verifyToken, catchError(history.createHistory))
+        .put(upload, verifyToken, catchError(history.updateHistory))
 
     app.route('/api/history/:id')
         .get(verifyToken, catchError(history.getHistory))
