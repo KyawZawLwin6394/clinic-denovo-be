620760d2ab65e6dd89deb4cc6224304d189637df
Kyaw Zaw Lwin <kyawzawlwin.work.6394@gmail.com>
2023-08-07 14:45:20 +0630

    here

--- app/controllers/treatmentVoucherController.js
+++ app/controllers/treatmentVoucherController.js
@@ -134,6 +134,22 @@ exports.getCode = async (req, res) => {
     }
 }
 
+exports.getCodeMS = async (req, res) => {
+    let data = {}
+    try {
+        let today = new Date().toISOString()
+        const latestDocument = await TreatmentVoucher.find({ tsType: 'MS' }, { seq: 1 }).sort({ _id: -1 }).limit(1).exec();
+        if (latestDocument.length === 0) data = { ...data, seq: 1, code: "MVC-" + today.split('T')[0].replace(/-/g, '') + "-1" } // if seq is undefined set initial patientID and seq
+        if (latestDocument.length > 0) {
+            const increment = latestDocument[0].seq + 1
+            data = { ...data, code: "MVC-" + today.split('T')[0].replace(/-/g, '') + "-" + increment, seq: increment }
+        }
+        return res.status(200).send({ success: true, data: data })
+    } catch (error) {
+        return res.status(500).send({ "error": true, message: error.message })
+    }
+}
+
 exports.createTreatmentVoucher = async (req, res, next) => {
     let data = req.body;
     try {
--- app/routes/treatmentVoucherRoutes.js
+++ app/routes/treatmentVoucherRoutes.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const { verify } = require("crypto");
 const treatmentVoucher = require("../controllers/treatmentVoucherController");
 const { catchError } = require("../lib/errorHandler");
 const verifyToken = require('../lib/verifyToken');
@@ -16,11 +17,13 @@ module.exports = (app) => {
         .post(verifyToken, catchError(treatmentVoucher.activateTreatmentVoucher))
 
     app.route('/api/treatment-vouchers')
-    .get(verifyToken, catchError(treatmentVoucher.listAllTreatmentVouchers))
+        .get(verifyToken, catchError(treatmentVoucher.listAllTreatmentVouchers))
     app.route('/api/treatment-vouchers/search')
         .post(verifyToken, catchError(treatmentVoucher.searchTreatmentVoucher))
     app.route('/api/treatment-vouchers/filter')
         .post(verifyToken, catchError(treatmentVoucher.getRelatedTreatmentVoucher))
+    app.route('/api/treatment-vouchers/code/ms')
+        .get(verifyToken, catchError(treatmentVoucher.getCodeMS))
     app.route('/api/treatment-vouchers/code').get(verifyToken, catchError(treatmentVoucher.getCode))
     app.route('/api/treatment-vouchers/today').get(verifyToken, catchError(treatmentVoucher.getTodaysTreatmentVoucher))
     app.route('/api/treatment-vouchers/get-date').get(verifyToken, catchError(treatmentVoucher.getwithExactDate))
